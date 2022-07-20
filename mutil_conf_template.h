dfb121b873c88b151d38bce8bde7ff7ea9a48b63
墨恒 <ylm200322400@163.com>
2022-07-20 15:31:16 +0800

    让配置文件支持CMSIS

--- mutil_conf_template.h
+++ mutil_conf_template.h
@@ -1,7 +1,21 @@
 #ifndef __SRC_MUTIL_CONF
 #define __SRC_MUTIL_CONF
 
-#define ENABLE_DRIVE 0
-#define ENABLE_MPACK 0
+// <<< Use Configuration Wizard in Context Menu >>>
+
+// <h> MUtil的功能开启配置
+
+// <e> 启用MUtil的stm32f1的工具函数
+// <i> 默认: 0
+#define ENABLE_DRIVE 1
+// </e>
+
+// <e> 启用MUtil的MPack封装
+// <i> 默认: 0
+#define ENABLE_MPACK 1
+// </e>
+
+// </h>
+// <<< end of configuration section >>>
 
 #endif
